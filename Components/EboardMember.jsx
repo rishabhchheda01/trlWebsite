5170df4db18a7ca84017b0866d754575f0241ed7
Soumya Jailwala <SoumyaJailwala@users.noreply.github.com>
2025-10-23 17:35:47 -0400

    eboard changes

--- Components/EboardMember.jsx
+++ Components/EboardMember.jsx
@@ -1,8 +1,8 @@
 export default function EboardMember({ name, position, image, description, className = "" }){
     return(
-        <div className={`flex flex-col items-center text-center border-2 border-[#E21833] bg-red-500/10 rounded-lg p-5 ${className}`}>
-            <img src={ image } alt={ name } className="w-20 h-20 rounded-full" />
-            <h2 className="text-2xl text-white font-geist">{ name }</h2>
+        <div className={`flex flex-col items-center text-center border-2 border-[#E21833] bg-red-500/10 rounded-lg p-5 h-90 overflow-y-auto ${className}`}>
+            <img src={ image } alt={ name } className="w-full max-w-48 aspect-square object-cover rounded-full" />
+            <h2 className="text-2xl text-white font-geist mt-4">{ name }</h2>
             <p className="text-lg text-white font-geist">{ position }</p>
             <p className="text-md text-white font-geist">{ description }</p>
         </div>
--- Pages/Eboard.jsx
+++ Pages/Eboard.jsx
@@ -5,6 +5,13 @@ export default function Eboard(){
     const menu_items = [{name: "Eboard", url: "/eboard"}, {name: "About", url: "/about"}]
     
     const eboardMembers = [
+
+        {
+            name: "Sarah Zhu",
+            position: "President",
+            image: "../Images/Eboard_Pics/Sarah.png",
+            description: "Sarah is a senior studying Computer Science with a minor in Sustainability Studies. She’s been with TRL for almost four years—starting out as a general member, moving into the Artist Relations Manager role, and now serving as President. Outside of TRL, you can catch her as the lead singer and guitarist of her band no girlfriend, where she channels her love for music and performing."
+        },
         {
             name: "Jolie Boiadjieva",
             position: "Artist Relations",
@@ -35,8 +42,8 @@ export default function Eboard(){
         <div>
             <Navigation menuItems={ menu_items } />
             <div className="p-5">
-                <h1 className="text-white w-[100%] text-center font-geist text-4xl m-5">Executive Board</h1>
-                <div className="flex flex-row justify-center gap-8 mt-5">
+                <h1 className="text-white w-[100%] text-center font-geist text-4xl m-7">Executive Board</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-5 justify-items-center">
                     {eboardMembers.map((member, index) => (
                         <EboardMember 
                             key={index}
@@ -44,7 +51,7 @@ export default function Eboard(){
                             position={member.position}
                             image={member.image}
                             description={member.description}
-                            className="w-72"
+                            className="w-full max-w-75"
                         />
                     ))}
                 </div>
