35160c6998863d7d2179b6b4ab9b8c9aa093c225
Rishabh <rchheda@umd.edu>
2025-11-04 18:47:33 -0500

    events merged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "/Users/manan/trl-website/trlWebsite/src/assets/trlTapeLogo.png";
+import logo from "../assets/trlTapeLogo.png";
 
 export default function Navbar() {
   return (
